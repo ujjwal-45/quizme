9847081e361332c0a51e6e81277edaa5c8c2767a
Ujjwal <ujjwalchoudhary065@gmail.com>
2023-08-15 19:28:19 +0530

    updated : sign In button

---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { signIn } from 'next-auth/react'
+import React from 'react'
+import { Button } from './ui/button'
+
+type Props = {
+    text: string;
+}
+
+const SignInButton = ({text}: Props) => {
+  return (
+      <div >
+          <Button  onClick={() => {
+              signIn('google').catch(console.error);
+              
+          }}>
+              {text}
+              
+          </Button>
+
+           
+      </div>
+      
+  )
+}
+
+export default SignInButton
